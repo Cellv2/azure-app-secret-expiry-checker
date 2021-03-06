9d288a0ed6ec613898f606215d274927b95bc11a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-03-06 16:52:34 +0000

    Add msal and dotenv

---
+++ src/secrets.ts
@@ -0,0 +1,7 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const TENANT_ID_DEV = process.env.TENANT_ID_DEV;
+export const CLIENT_ID_DEV = process.env.CLIENT_ID_DEV;
+export const CLIENT_SECRET_DEV = process.env.CLIENT_SECRET_DEV;
+export const CLIENT_SECRETID_DEV = process.env.CLIENT_SECRETID_DEV;
