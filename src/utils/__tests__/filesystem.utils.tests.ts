c90d61c9589024e944b05d0cb88cb585e94064ea
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-03-18 18:12:58 +0000

    :construction: Begin testing

---
+++ src/utils/__tests__/filesystem.utils.tests.ts
@@ -0,0 +1,8 @@
+import { mocked } from 'ts-jest/utils'
+import { ensureDirExistsForWritesAsync } from '../filesystem.utils'
+
+describe("utils - ensureDirExistsForWritesAsync", () => {
+    const mockedFunction = mocked(ensureDirExistsForWritesAsync);
+
+    // it("")
+})
