9219e39df0c4cb5420f3df99624a908ed0b9ea79
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-04-04 12:55:57 +0100

    :recycle: Move question prompts into own file

---
+++ src/cli/index.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+import dataRequestorInstance from "../data/data-requestor";
+import DataStore, { DataStoreInterface } from "../data/data-store";
+import { CliAnswers } from "../types/cli.types";
+import { Data } from "../types/data.types";
+import { cliQuestions } from "./questions";
+
+// main entry point for all questions prompted for user input
+export const askQuestions = (): void => {
+    let store: DataStoreInterface;
+    inquirer
+        .prompt(cliQuestions)
+        .then((answers: CliAnswers) => {
+            console.log(JSON.stringify(answers, null, 4));
+            if (answers.singleOrMultipleInput === "single") {
+                const dataObj: Data = {
+                    clientId: answers.singleInputConfigClientId,
+                    clientSecret: answers.singleInputConfigClientSecret,
+                    serviceToUse: answers.singleInputConfigServiceToUse,
+                    tenantId: answers.singleInputConfigTenantId,
+                };
+
+                store = new DataStore(dataObj);
+            }
+
+            if (answers.singleOrMultipleInput === "multiple") {
+                if (answers.multipleInputDataLocation === "cliArray") {
+                    store = new DataStore(answers.multipleInputCliArray);
+                }
+            }
+
+            // TODO: imeplement this
+            if (answers.multipleInputDataLocation === "ftp") {
+                console.log("NYI");
+            }
+
+            // TODO: imeplement this
+            if (answers.multipleInputDataLocation === "localFile") {
+                console.log("NYI");
+            }
+        })
+        .then(() => {
+            dataRequestorInstance.requestSecretsForAllApps(
+                store.getDataObjectsToCheck()
+            );
+        });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,8 @@
-import inquirer from "inquirer";
-import { cliQuestions } from "./cli/questions";
+import { askQuestions } from "./cli";
 import Config from "./config/config";
-import dataRequestorInstance from "./data/data-requestor";
-import DataStore, { DataStoreInterface } from "./data/data-store";
-import { testData } from "./data/testData";
 import filesystemInterfaceInstance from "./services/filesystem/filesystem-interface.service";
-import AadGraphFunctions from "./services/microsoft/aad-graph.functions";
-import { AadGraphService } from "./services/microsoft/aad-graph.service";
-import { MicrosoftServiceBaseInterface } from "./services/microsoft/microsoft-service-base";
-import MsGraphFunctions from "./services/microsoft/ms-graph.functions";
-import { MsGraphService } from "./services/microsoft/ms-graph.service";
-import { CliAnswers } from "./types/cli.types";
-import { Data } from "./types/data.types";
-import { ClientConnection } from "./types/microsoft-service.types";
 
 console.log("heya!");
-
-const main = async (): Promise<void> => {
-    const data = testData;
-    data.forEach(async (item) => {
-        const { clientId, clientSecret, tenantId, serviceToUse } = item;
-
-        const clientConnection: ClientConnection = {
-            clientId,
-            clientSecret,
-            tenantId,
-        };
-
-        let service: MicrosoftServiceBaseInterface;
-        if (serviceToUse === "MsGraph") {
-            service = new MsGraphService();
-            MsGraphFunctions.getSecretsForMsGraphApplicationsByAppId(
-                service,
-                clientConnection,
-                clientId
-            );
-        } else {
-            service = new AadGraphService();
-            AadGraphFunctions.getSecretsForAadGraphApplicationsByAppId(
-                service,
-                clientConnection,
-                clientId
-            );
-        }
-    });
-};
 
 const mainFs = () => {
     const config = new Config("", "", "");
@@ -53,49 +11,10 @@ const mainFs = () => {
 
     filesystemInterfaceInstance.writeDataToFilesystemAsync("", "");
 };
-
-// main();
-
 // mainFs();
 
-const inq = () => {
-    let store: DataStoreInterface;
-    inquirer
-        .prompt(cliQuestions)
-        .then((answers: CliAnswers) => {
-            console.log(JSON.stringify(answers, null, 4));
-            if (answers.singleOrMultipleInput === "single") {
-                const dataObj: Data = {
-                    clientId: answers.singleInputConfigClientId,
-                    clientSecret: answers.singleInputConfigClientSecret,
-                    serviceToUse: answers.singleInputConfigServiceToUse,
-                    tenantId: answers.singleInputConfigTenantId,
-                };
-
-                store = new DataStore(dataObj);
-            }
-
-            if (answers.singleOrMultipleInput === "multiple") {
-                if (answers.multipleInputDataLocation === "cliArray") {
-                    store = new DataStore(answers.multipleInputCliArray);
-                }
-            }
-
-            // TODO: imeplement this
-            if (answers.multipleInputDataLocation === "ftp") {
-                console.log("NYI");
-            }
-
-            // TODO: imeplement this
-            if (answers.multipleInputDataLocation === "localFile") {
-                console.log("NYI");
-            }
-        })
-        .then(() => {
-            dataRequestorInstance.requestSecretsForAllApps(
-                store.getDataObjectsToCheck()
-            );
-        });
+const main = async (): Promise<void> => {
+    askQuestions();
 };
 
-inq();
+main();
