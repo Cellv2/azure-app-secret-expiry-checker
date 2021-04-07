7492b394268dfa026895ae00b69eee774a0422cd
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-04-07 21:51:21 +0100

    :white_check_mark: Add tests

--- src/utils/__tests__/filesystem.utils.test.ts
+++ src/utils/__tests__/filesystem.utils.test.ts
@@ -1,18 +1,36 @@
 import fs from "fs";
 import path from "path";
 import { mocked } from "ts-jest/utils";
-import { ensureDirExistsForWritesAsync } from "../filesystem.utils";
+import {
+    checkFileExistsForRead,
+    ensureDirExistsForWritesAsync,
+} from "../filesystem.utils";
 
 const fixtureDir = path.resolve(__dirname, "./__fixtures__");
 const newFixtureDir = path.resolve(__dirname, "./__fixtures__/tmp");
-const fixtureFile = path.resolve(
+
+const validFixtureFile = path.resolve(
     __dirname,
     "./__fixtures__/filesystem-test-data.json"
 );
+const invalidFixtureFile = path.resolve(__dirname, `${Symbol().toString()}`);
+
+describe("utils - checkFileExistsForRead", () => {
+    const mockedFunction = mocked(checkFileExistsForRead);
+
+    it("returns true for an existing file", () => {
+        expect(mockedFunction(validFixtureFile)).toBeTruthy();
+    });
+
+    it("returns false for a missing file", () => {
+        expect(mockedFunction(invalidFixtureFile)).toBeFalsy();
+    });
+});
 
 describe("utils - ensureDirExistsForWritesAsync", () => {
     afterAll(() => {
         // TODO: uncomment this when #3 is fixed
+        // TODO: also make sure this isn't eating other files (I think it deleted filesystem-test-data.json ?)
         // rimraf(`${fixtureDir}/*`, (err) => console.error(err));
     });
 
