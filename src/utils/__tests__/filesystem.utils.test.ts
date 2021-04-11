e52c421fb85c6792212d885186a6ba709b9c732e
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-04-11 14:10:24 +0100

    :white_check_mark: Add tests

--- src/utils/__tests__/filesystem.utils.test.ts
+++ src/utils/__tests__/filesystem.utils.test.ts
@@ -5,32 +5,54 @@ import rimraf from "rimraf";
 import { mocked } from "ts-jest/utils";
 import {
     checkFileExistsForRead,
+    checkFileHasValidData,
     createDirIfNotExistsAsync,
     doesDirExistsForWritesAsync,
 } from "../filesystem.utils";
 
 const TEST_DIR_ROOT = path.resolve(__dirname, "./__tempTestDir");
 
-const existingDir = path.resolve(__dirname, "./__fixtures__");
-const fixtureDir = path.resolve(__dirname, "./__fixtures__");
-const newFixtureDir = path.resolve(__dirname, "./__fixtures__/tmp");
-const invalidFixtureDir = path.resolve(__dirname, "./__fixtures__/tmp");
-
-const validFixtureFile = path.resolve(
+const fixurePath_valid = path.resolve(
     __dirname,
-    "./__fixtures__/filesystem-test-data.json"
+    "./__fixtures__/fs-data_valid.json"
 );
-const invalidFixtureFile = path.resolve(__dirname, `${Symbol().toString()}`);
+const fixurePath_valid2 = path.resolve(
+    __dirname,
+    "./__fixtures__/fs-data_valid-2.json"
+);
+const fixurePath_validAsTxt = path.resolve(
+    __dirname,
+    "./__fixtures__/fs-data_valid-txtFIle.txt"
+);
+const fixurePath_invalidKeys = path.resolve(
+    __dirname,
+    "./__fixtures__/fs-data_invalid-keys.json"
+);
+const fixurePath_invalidNotArr = path.resolve(
+    __dirname,
+    "./__fixtures__/fs-data_invalid-notArr.json"
+);
+const fixurePath_invalidNotValidJson = path.resolve(
+    __dirname,
+    "./__fixtures__/fs-data_invalid-notValidJson.json"
+);
 
 describe("utils - checkFileExistsForRead", () => {
     const mockedFunction = mocked(checkFileExistsForRead);
 
     it("returns true for an existing file", () => {
-        expect(mockedFunction(validFixtureFile)).toBeTruthy();
+        expect(mockedFunction(fixurePath_valid)).toBeTruthy();
     });
 
     it("returns false for a missing file", () => {
-        expect(mockedFunction(invalidFixtureFile)).toBeFalsy();
+        const DIR_ID = nanoid(10);
+        const invalidFilePath = path.resolve(
+            TEST_DIR_ROOT,
+            DIR_ID,
+            `${nanoid(5)}.json`
+        );
+
+        expect(mockedFunction(invalidFilePath)).toBeFalsy();
     });
 });
 
@@ -39,12 +61,17 @@ describe("utils - doesDirExistsForWritesAsync", () => {
 
     it("returns true for an existing directory", async () => {
         expect.assertions(1);
-        await expect(mockedFunction(fixtureDir)).resolves.toBeTruthy();
+
+        await expect(mockedFunction(TEST_DIR_ROOT)).resolves.toBeTruthy();
     });
 
     it("returns false for a missing directory", async () => {
+        const DIR_ID = nanoid(10);
+        const invalidFilePath = path.resolve(TEST_DIR_ROOT, DIR_ID);
+
         expect.assertions(1);
-        await expect(mockedFunction(invalidFixtureDir)).resolves.toBeFalsy();
+
+        await expect(mockedFunction(invalidFilePath)).resolves.toBeFalsy();
     });
 });
 
@@ -116,6 +143,72 @@ describe("utils - createDirIfNotExistsAsync", () => {
         );
         const invalidDirPath = `${partialInvalidDirPath}\000`;
         const errorString = `Unable to create directory at ${invalidDirPath}`;
+
+        expect.assertions(1);
+
+        await expect(mockedFunction(invalidDirPath)).rejects.toThrowError(
+            errorString
+        );
+    });
+});
+
+describe("utils - checkFileHasValidData", () => {
+    const mockedFunction = mocked(checkFileHasValidData);
+
+    it("resolves if the data in the file is valid", async () => {
+        expect.assertions(3);
+
+        await expect(mockedFunction(fixurePath_valid)).resolves.not.toThrow();
+        await expect(mockedFunction(fixurePath_valid2)).resolves.not.toThrow();
+        await expect(
+            mockedFunction(fixurePath_validAsTxt)
+        ).resolves.not.toThrow();
+    });
+
+    it("throws if data in the file is invalid", async () => {
+        expect.assertions(3);
+
+        await expect(
+            mockedFunction(fixurePath_invalidKeys)
+        ).rejects.toThrowError("Keys do not match");
+        await expect(
+            mockedFunction(fixurePath_invalidNotArr)
+        ).rejects.toThrowError(
+            "Please ensure the file contains an array of data"
+        );
+        await expect(
+            mockedFunction(fixurePath_invalidNotValidJson)
+        ).rejects.toThrowError(
+            "Please ensure that the input is *valid* JSON (e.g. all keys are quoted)"
+        );
+    });
+
+    it("does not create the path or file if incorrectly called", async () => {
+        const DIR_ID = nanoid(10);
+        const invalidPath = path.resolve(TEST_DIR_ROOT, DIR_ID);
+        const invalidFile = path.resolve(TEST_DIR_ROOT, DIR_ID, "test.json");
+
+        expect.assertions(4);
+
+        await expect(mockedFunction(invalidPath)).rejects.toThrowError(
+            `Something went wrong when reading file at ${invalidPath}`
+        );
+        await expect(fs.promises.access(invalidPath)).rejects.toThrow();
+
+        await expect(mockedFunction(invalidFile)).rejects.toThrowError(
+            `Something went wrong when reading file at ${invalidFile}`
+        );
+        await expect(fs.promises.stat(invalidFile)).rejects.toThrow();
+    });
+
+    it("throws when an invalid path is provided", async () => {
+        // node does not allow us to explicitly use octal escape sequences directly, so we split it up
+        const partialInvalidDirPath = path.resolve(
+            TEST_DIR_ROOT,
+            "./subfolder/"
+        );
+        const invalidDirPath = `${partialInvalidDirPath}\000\\test.json`;
+        const errorString = `Something went wrong when reading file at ${invalidDirPath}`;
 
         expect.assertions(1);
 
