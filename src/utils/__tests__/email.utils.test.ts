a0a53fba1550fc6ae86f92dfffb1c441d64e6a0e
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-04-24 12:52:49 +0100

    :white_check_mark: Add tests

--- src/utils/__tests__/email.utils.test.ts
+++ src/utils/__tests__/email.utils.test.ts
@@ -6,6 +6,9 @@ import {
 import { mocked } from "ts-jest/utils";
 import { TABLE_HEADER_KEYS } from "../../constants/email.constants";
 
+import fs from "fs";
+import path from "path";
+
 const testHeaderKeys = [
     "displayName",
     "keyId",
@@ -14,12 +17,26 @@ const testHeaderKeys = [
     "hint",
 ];
 
+const testData = [
+    {
+        displayName: "App_1",
+        endDateTime: "Some time in the future",
+        keyId: "a guid",
+        hint: "some hint",
+    },
+    {
+        displayName: "App_2",
+        endDateTime: "some time in the past",
+        keyId: "another guid",
+    },
+];
+
 describe("email utils", () => {
     describe("generateTableHeaders", () => {
         const mockedFunction = mocked(generateTableHeaders);
 
         it("produces the expected output", () => {
-            //@ts-ignore - we know this is incorrectly typed, but it should still work regardless of input
+            //@ts-expect-error - we know this is incorrectly typed, but it should still work regardless of input
             expect(mockedFunction(testHeaderKeys)).toStrictEqual<string>(
                 '<tr style="border-bottom:1px solid #ecedee;text-align:left;"><th>displayName</th>,<th>keyId</th>,<th>endDateTime</th>,<th>customKeyIdentifier</th>,<th>hint</th></tr>'
             );
@@ -33,23 +50,9 @@ describe("email utils", () => {
     describe("generateTableRows", () => {
         const mockedFunction = mocked(generateTableRows);
 
-        const testData = [
-            {
-                displayName: "App_1",
-                endDateTime: "Some time in the future",
-                keyId: "a guid",
-                hint: "some hint",
-            },
-            {
-                displayName: "App_2",
-                endDateTime: "some time in the past",
-                keyId: "another guid",
-            },
-        ];
-
         it("returns the correct rows depending on the columns provided", () => {
             expect(
-                //@ts-ignore
+                //@ts-expect-error - we expect this to error as the header keys are incorrectly typed
                 mockedFunction(testHeaderKeys, testData)
             ).toStrictEqual<string>(
                 "<tr><td>App_1</td>,<td>a guid</td>,<td>Some time in the future</td>,<td>null</td>,<td>some hint</td></tr><tr><td>App_2</td>,<td>another guid</td>,<td>some time in the past</td>,<td>null</td>,<td>null</td></tr>"
@@ -60,6 +63,25 @@ describe("email utils", () => {
             ).toStrictEqual<string>(
                 "<tr><td>App_1</td>,<td>a guid</td>,<td>Some time in the future</td></tr><tr><td>App_2</td>,<td>another guid</td>,<td>some time in the past</td></tr>"
             );
+        });
+    });
+
+    describe("generateMjmlTable", async () => {
+        const mockedFunction = mocked(generateMjmlTable);
+
+        // would prefer actual HTML comparison rather than snapshots, but the line breaks are acting funny
+        it("produces the expected output", async () => {
+            //@ts-expect-error - we expect this to error as the header keys are incorrectly typed
+            const testHeadersParse = mockedFunction(testHeaderKeys, testData);
+            expect(testHeadersParse.errors).toHaveLength(0);
+            expect(testHeadersParse.html).toMatchSnapshot();
+
+            const actualHeadersParse = mockedFunction(
+                TABLE_HEADER_KEYS,
+                testData
+            );
+            expect(actualHeadersParse.errors).toHaveLength(0);
+            expect(actualHeadersParse.html).toMatchSnapshot();
         });
     });
 });
